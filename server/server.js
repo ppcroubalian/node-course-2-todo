6636a92a4ebf28725187d308a86f5255f276c3c8
ppcroubalian <ppcroubalian@gmail.com>
2019-01-08 21:24:11 -0500

    Add GET /todos Route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos', (req,res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  })
+}, (e) => {
+  res.status(400).send(e);
+});
+
 app.listen(3000, () => {
   console.log('Server started on .... wait for it .... port 3000');
 });
