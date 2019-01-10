0a88ed6fe49bb76c3ce2e6732da89fb2e804c99b
ppcroubalian <ppcroubalian@gmail.com>
2019-01-09 19:28:04 -0500

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +29,29 @@ app.get('/todos', (req, res) => {
 }, (e) => {
   res.status(400).send(e);
 });
+
+// GET /todos.12345
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({
+      error: `Not Found: ID ${id} is Invalid`,
+      status: 404
+    });
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({
+        error: `Not Found: ID ${id} is NOT in Dataset`,
+        status: 404
+      });
+    }
+    res.send({todo});
+  })
+}, (e) => {
+  res.status(400).send(e);
+});
+
 
 app.listen(3000, () => {
   console.log('Server started on .... wait for it .... port 3000');
