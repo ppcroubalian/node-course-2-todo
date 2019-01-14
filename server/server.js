d549307ffc3bcae2712897fa1ba90f63ed92b796
ppcroubalian <ppcroubalian@gmail.com>
2019-01-13 19:51:59 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 
@@ -71,6 +73,35 @@ app.delete('/todos/:id', (req,res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body,['text', 'completed']);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({
+      error: `ID Not Found: ${id} is Invalid`,
+      status: 404
+    });
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    bodyCompletedAt = null;
+  }
+  Todo.findOneAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({
+        error: `Not Found: ID ${id} is NOT in Dataset`,
+        status: 404
+      });
+    }
+    res.send({todo});
+  })
+}, (e) => {
+  res.status(400).send(e);
 });
 
 const port = process.env.PORT || 3000;
